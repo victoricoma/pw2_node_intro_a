2e187b7b3aaf64ccf3b208a3d4d1276331df7efc
hadriansilva-cps <hadrian.silva01@etec.sp.gov.br>
2024-02-28 20:40:59 -0300

    Aula2802 modulos internos

---
+++ aula2802/moduloCalculadora.js
@@ -0,0 +1,29 @@
+module.exports = {calculadora(n1 ,n2 , op){
+
+
+
+    switch(op){
+        case "+" :
+            return(n1 + n2).toFixed(2)
+        case "-":
+            return(n1 - n2).toFixed(2)
+        case "*":
+            return(n1 * n2).toFixed(2)
+        case "/":
+            return(n1 / n2).toFixed(2)
+        default:
+            console.log("Invalid Operation!")
+
+
+        }
+   },
+   cientifica(n1 ,n2 , op){
+    switch(op){
+        case "sr":
+            return(math.sqrt(n1 + n2))
+            default:
+                console.log("invalid Operation!")
+    }
+   }
+}
+
